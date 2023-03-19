74a217f6bbc1ce64a13ba2b000311d83049550a5
3dzbot <zoduaks@gmail.com>
2023-03-19 23:23:52 +0200

    3.02 BundleAnalyzer

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,6 +39,7 @@ module.exports = {
         'no-unused-vars': 'warn',
         'react/jsx-props-no-spreading': 'warn',
         'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'max-len': ['error', { ignoreComments: true, code: 100 }],
     },
     globals: {
         __IS_DEV__: true,
