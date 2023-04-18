a5fb8ddf861bedacc6cdffa33db222a7158a3496
3dzbot <zoduaks@gmail.com>
2023-04-19 01:19:47 +0300

    3.05 Настраиваем Storybook. Декораторы. Стори кейсы на компоненты

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,8 +1,6 @@
 import webpack from 'webpack';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import { types } from 'sass';
 import { BuildOptions } from './types/config';
-import Boolean = types.Boolean;
+import { buildCssLoader } from './loaders/buildCssLoader';
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     const svgLoader = {
@@ -39,26 +37,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         },
     };
 
-    const cssLoader = {
-        test: /\.s[ac]ss$/i,
-        use: [
-            // Creates `style` nodes from JS strings
-            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-            // Translates CSS into CommonJS
-            {
-                loader: 'css-loader',
-                options: {
-                    modules: {
-                        auto: (resPath: string) => resPath.includes('.module.'),
-                        localIdentName: isDev ? '[path][name]__[local]--[hash:base64:5]' : '[hash:base64:5]',
-                    },
-
-                },
-            },
-            // Compiles Sass to CSS
-            'sass-loader',
-        ],
-    };
+    const cssLoader = buildCssLoader(isDev);
 
     const typescriptLoader = {
         test: /\.tsx?$/,
--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -17,16 +17,36 @@ export default {
 
     // Automatically clear mock calls, instances and results before every test
     clearMocks: true,
-
+    // The test environment that will be used for testing
+    testEnvironment: 'jsdom',
     // An array of regexp pattern strings used to skip coverage collection
     coveragePathIgnorePatterns: [
         '\\\\node_modules\\\\',
     ],
+    // An array of file extensions your modules use
+    moduleFileExtensions: [
+        'js',
+        'jsx',
+        'ts',
+        'tsx',
+        'json',
+        'node',
+    ],
+    // An array of directory names to be searched recursively up from the requiring module's location
+    moduleDirectories: [
+        'node_modules',
+    ],
     modulePaths: [
         '<rootDir>src',
     ],
+    // The glob patterns Jest uses to detect test files
+    testMatch: [
+        '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
+    ],
+    // The root directory that Jest should scan for tests and modules within
+    rootDir: '../../',
     // In jest.config.js add (if you haven't already)
-    setupFilesAfterEnv: ['<rootDir>config/jest/setupTest.ts'],
+    setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
 
     moduleNameMapper: {
         '\\.s?css$': 'identity-obj-proxy',
@@ -77,21 +97,6 @@ export default {
     // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
     // maxWorkers: "50%",
 
-    // An array of directory names to be searched recursively up from the requiring module's location
-    moduleDirectories: [
-        'node_modules',
-    ],
-
-    // An array of file extensions your modules use
-    moduleFileExtensions: [
-        'js',
-        'jsx',
-        'ts',
-        'tsx',
-        'json',
-        'node',
-    ],
-
     // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
     // moduleNameMapper: {},
 
@@ -125,9 +130,6 @@ export default {
     // Automatically restore mock state and implementation before every test
     // restoreMocks: false,
 
-    // The root directory that Jest should scan for tests and modules within
-    rootDir: '../../',
-
     // A list of paths to directories that Jest should use to search for files in
     // roots: [
     //   "<rootDir>"
@@ -148,19 +150,11 @@ export default {
     // A list of paths to snapshot serializer modules Jest should use for snapshot testing
     // snapshotSerializers: [],
 
-    // The test environment that will be used for testing
-    testEnvironment: 'jsdom',
-
     // Options that will be passed to the testEnvironment
     // testEnvironmentOptions: {},
 
     // Adds a location field to test results
     // testLocationInResults: false,
-
-    // The glob patterns Jest uses to detect test files
-    testMatch: [
-        '<rootDir>src/**/*(*.)@(spec|test).[jt]s?(x)',
-    ],
 
     // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
     // testPathIgnorePatterns: [
