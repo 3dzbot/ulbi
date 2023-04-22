d026496fcc39ff45b57b9ba3f4111df7498364ab
3dzbot <zoduaks@gmail.com>
2023-04-22 13:05:04 +0300

    3.08 Сайдбар. Состояния кнопки

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,21 +1,14 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
-import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
     className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => {
-    const { t } = useTranslation();
-
-    return (
-        <div className={classNames(cls.Navbar, {}, [className])}>
-            <div className={cls.links}>
-                <AppLink to="/" theme={AppLinkTheme.SECONDARY}>{t('Главная')}</AppLink>
-                <AppLink to="/about" theme={AppLinkTheme.SECONDARY}>{t('О нас')}</AppLink>
-            </div>
+export const Navbar = ({ className }: NavbarProps) => (
+    <div className={classNames(cls.Navbar, {}, [className])}>
+        <div className={cls.links}>
+            /
         </div>
-    );
-};
+    </div>
+);
